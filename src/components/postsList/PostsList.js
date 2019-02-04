cd35612c2c87bc39bc53180a987f7889d5d9cad5
Rani <ranihorev@gmail.com>
2019-02-03 19:12:49 -0800

    Reorg classes

--- src/components/postsList/PostsList.js
+++ src/components/postsList/PostsList.js
@@ -3,12 +3,23 @@ import {TransitionGroup, CSSTransition} from 'react-transition-group';
 import './postsList.scss';
 import Post from "../post/Post";
 import PropTypes from 'prop-types';
+import {fetchPostsAction} from "../../actions/action_posts";
+import {connect} from "react-redux";
+import {withRouter} from "react-router";
+import {isEmpty} from "lodash";
 
 
-export default class PostsList extends Component {
+class PostsList extends Component {
+
+  componentDidMount() {
+    this.props.fetchPosts(this.props.match.params);
+  }
+
   render() {
-    const {posts} = this.props;
-    const posts_dom = posts.map((p) =>
+    const {data} = this.props.posts;
+    if (isEmpty(data)) return <div></div>
+
+    const posts_dom = data.map((p) =>
       <CSSTransition key={p._id} in={this.props.in} timeout={400} classNames="fade" appear={false}>
         <Post content={p}/>
       </CSSTransition>
@@ -21,6 +32,18 @@ export default class PostsList extends Component {
   }
 }
 
-PostsList.propTypes = {
-  posts: PropTypes.array
-};
+const mapStateToProps = (state, ownProps) => {
+  return {
+    posts: state.posts
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    fetchPosts: (params) => {
+      dispatch(fetchPostsAction(params));
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (withRouter(PostsList));
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,10 @@ import React, { Component } from 'react';
 import {Container, Row, Col} from "reactstrap";
 import BaseNavbar from "../components/navbar/navbar";
 import NewPost from "../components/newPost/NewPost";
-import {connect} from "react-redux";
-import {fetchPostsAction} from "../actions/action_posts";
 import PostsList from "../components/postsList/PostsList";
-import {isEmpty} from "lodash";
 
 
 class Home extends Component {
-
-  componentDidMount() {
-    this.props.fetchPosts(this.props.match.params);
-  }
 
   render() {
     const {posts} = this.props;
@@ -23,10 +16,7 @@ class Home extends Component {
           <Row className="justify-content-center">
             <Col xs="12" md="8" lg="7">
               <NewPost/>
-              {!isEmpty(posts) && !isEmpty(posts.data) ?
-                <PostsList posts={posts.data}/> :
-                ''
-              }
+              <PostsList/>
             </Col>
           </Row>
         </Container>
@@ -35,19 +25,5 @@ class Home extends Component {
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    posts: state.posts
-  }
-}
-
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    fetchPosts: (params) => {
-      dispatch(fetchPostsAction(params));
-    },
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps) (Home);
+export default Home;
 
